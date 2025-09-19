5e354c1ad38fd2937c00cd3f182313b9a49d0eb1
Abinav <mrabinavkadel@gmail.com>
2025-09-18 23:08:29 -0400

    declared character variables with different data types

---
+++ Assignment_1/sillyCharacter.js
@@ -0,0 +1,6 @@
+// Character attributes
+let characterName = "Captain Waffles";  
+let age = 25;                           
+let isSuperhero = true;                 
+let specialPowers = ["flying", "invisibility", "super strength", "teleportation"]; 
+let favoriteFood = "Pizza";             
