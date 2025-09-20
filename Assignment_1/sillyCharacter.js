f2480dcc35e1e22d5ba0a081ffdc83bfd9896011
Abinav <mrabinavkadel@gmail.com>
2025-09-20 14:22:22 -0400

    adding random age generation using Math.random

--- Assignment_1/sillyCharacter.js
+++ Assignment_1/sillyCharacter.js
@@ -1,6 +1,5 @@
-// Character attributes
-let characterName = "Captain Waffles";  
-let age = 25;                           
-let isSuperhero = true;                 
-let specialPowers = ["flying", "invisibility", "super strength", "teleportation"]; 
+let characterName = "Captain Waffles";
+let age = Math.floor(Math.random() * 60) + 10; // random age between 10–70
+let isSuperhero = true;
+let specialPowers = ["flying", "invisibility", "super strength", "teleportation"];
 let favoriteFood = "Pizza";             
