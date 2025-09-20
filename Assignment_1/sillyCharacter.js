5ad1d32b13df757b463959065a4456d611796298
Abinav <mrabinavkadel@gmail.com>
2025-09-20 14:25:14 -0400

    created generateDescription function with template

--- Assignment_1/sillyCharacter.js
+++ Assignment_1/sillyCharacter.js
@@ -2,4 +2,16 @@ let characterName = "Captain Waffles";
 let age = Math.floor(Math.random() * 60) + 10; // random age between 10–70
 let isSuperhero = true;
 let specialPowers = ["flying", "invisibility", "super strength", "teleportation"];
-let favoriteFood = "Pizza";             
+let favoriteFood = "Pizza";      
+
+function generateDescription() {
+    let randomPower = specialPowers[Math.floor(Math.random() * specialPowers.length)];
+    let heroStatus;
+if (isSuperhero) {
+    heroStatus = "a superhero";
+} else {
+    heroStatus = "just a regular person";
+}
+    return `Meet ${characterName}, a ${age}-year-old ${heroStatus} 
+            who loves ${favoriteFood} and can ${randomPower}!`;
+}
