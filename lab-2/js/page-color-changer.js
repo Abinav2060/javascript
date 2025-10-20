3b3f85a673df2a5a249b667388f3e67a76baab34
Abinav2060 <mrabinavkadel@gmail.com>
2025-10-19 22:18:52 -0400

    Initialize constants for sliders and HTML body element

---
+++ lab-2/js/page-color-changer.js
@@ -0,0 +1,6 @@
+ // Get references to DOM elements ======
+const redSlider = document.getElementById("red");
+const greenSlider = document.getElementById("green");
+const blueSlider = document.getElementById("blue");
+const body = document.querySelector("html");
+
