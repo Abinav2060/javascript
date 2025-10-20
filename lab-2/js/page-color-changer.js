ea0061a625dc11c613224069309257d219d66287
Abinav2060 <mrabinavkadel@gmail.com>
2025-10-19 22:24:29 -0400

    adding function to update the background color

--- lab-2/js/page-color-changer.js
+++ lab-2/js/page-color-changer.js
@@ -1,6 +1,15 @@
- // Get references to DOM elements ======
+ // getting references to DOM elements 
 const redSlider = document.getElementById("red");
 const greenSlider = document.getElementById("green");
 const blueSlider = document.getElementById("blue");
 const body = document.querySelector("html");
 
+// using function to update background color 
+function updateBackgroundColor() {
+  const red = redSlider.value;
+  const green = greenSlider.value;
+  const blue = blueSlider.value;
+
+  // applying the color to the page background
+  body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+}
