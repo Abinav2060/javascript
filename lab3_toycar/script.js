362f290ee6739453944114c46ef40f96ed130a4b
Abinav2060 <mrabinavkadel@gmail.com>
2025-11-16 22:32:40 -0500

    creating JavaScript constructor

---
+++ lab3_toycar/script.js
@@ -0,0 +1,8 @@
+function ToyCar(model, scale, color, manufacturer, price, batteryPowered) {
+  this.model = model;
+  this.scale = scale;
+  this.color = color;
+  this.manufacturer = manufacturer;
+  this.price = price;
+  this.batteryPowered = batteryPowered;
+}
