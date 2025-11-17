198b4aac9e3d45a8c1264b09328c189f8c5ce8c8
Abinav2060 <mrabinavkadel@gmail.com>
2025-11-16 22:47:06 -0500

    Adding method to display a ToyCar

--- lab3_toycar/script.js
+++ lab3_toycar/script.js
@@ -6,3 +6,17 @@ function ToyCar(model, scale, color, manufacturer, price, batteryPowered) {
   this.price = price;
   this.batteryPowered = batteryPowered;
 }
+function displayCar(car) {
+  const container = document.getElementById('carContainer');
+  const div = document.createElement('div');
+  div.className = 'car-card';
+  div.innerHTML = `
+    <p><strong>Model:</strong> ${car.model}</p>
+    <p><strong>Scale:</strong> ${car.scale}</p>
+    <p><strong>Color:</strong> ${car.color}</p>
+    <p><strong>Manufacturer:</strong> ${car.manufacturer}</p>
+    <p><strong>Price:</strong> $${car.price}</p>
+    <p><strong>Battery Powered:</strong> ${car.batteryPowered ? 'Yes' : 'No'}</p>
+  `;
+  container.appendChild(div);
+}
