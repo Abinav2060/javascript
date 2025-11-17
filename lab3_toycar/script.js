e267ff6c7f04bb89536a43e8ee13b711ab5c498a
Abinav2060 <mrabinavkadel@gmail.com>
2025-11-16 22:54:16 -0500

    final styling and bug fixes

--- lab3_toycar/script.js
+++ lab3_toycar/script.js
@@ -1,11 +1,17 @@
-function ToyCar(model, scale, color, manufacturer, price, batteryPowered) {
+function ToyCar(model, scale, color, manufacturer, price, batteryPowered, releaseYear, material, weight, rating, stockStatus) {
   this.model = model;
   this.scale = scale;
   this.color = color;
   this.manufacturer = manufacturer;
   this.price = price;
   this.batteryPowered = batteryPowered;
+  this.releaseYear = releaseYear;
+  this.material = material;
+  this.weight = weight;
+  this.rating = rating;
+  this.stockStatus = stockStatus;
 }
+
 function displayCar(car) {
   const container = document.getElementById('carContainer');
   const div = document.createElement('div');
@@ -22,12 +28,17 @@ function displayCar(car) {
 }
 document.getElementById('addCarBtn').addEventListener('click', function() {
   const toyCar = new ToyCar(
-    'Ferrari SF90 Stradale',
-    '1:18',
-    'Red',
-    'Hot Wheels',
-    199.99,
-    false
-  );
+  'Ferrari SF90 Stradale',
+  '1:18',
+  'Red',
+  'Hot Wheels',
+  199.99,
+  false,
+  2022,
+  'Metal Diecast',
+  '700g',
+  4.8,
+  'In Stock'
+);
   displayCar(toyCar);
 });
